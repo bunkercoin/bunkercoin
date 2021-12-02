490f49ffe35039f07e350366d6a35a52801134de
Bram <bramdevogel@kpnmail.nl>
2021-12-02 17:58:09 +0100

    Maybe fix binbloat

--- contrib/compilepackages-mingw.sh
+++ contrib/compilepackages-mingw.sh
@@ -22,13 +22,13 @@ mkdir -p mingw-bin/lib
 wget "https://www.openssl.org/source/old/1.0.2/openssl-1.0.2u.tar.gz"
 wget "http://miniupnp.free.fr/files/miniupnpc-2.0.20161216.tar.gz"
 wget "http://download.oracle.com/berkeley-db/db-5.1.29.NC.tar.gz"
-wget "https://deac-ams.dl.sourceforge.net/project/boost/boost/1.55.0/boost_1_65_0.tar.gz"
+wget "https://boostorg.jfrog.io/artifactory/main/release/1.65.1/source/boost_1_65_1.tar.gz"
 
 # Extract source code
 tar -xf openssl-1.0.2u.tar.gz
 tar -xf miniupnpc-2.0.20161216.tar.gz
 tar -xf db-5.1.29.NC.tar.gz
-tar -xf boost_1_55_0.tar.gz
+tar -xf boost_1_65_1
 
 rm -f openssl-1.0.2u.tar.gz miniupnpc-2.0.20161216.tar.gz db-5.1.29.NC.tar.gz boost_1_55_0.tar.gz
 
@@ -56,10 +56,12 @@ make -j`nproc`
 make install
 
 # Build libboost
-cd $HOME/boost_1_65_0
+cd $HOME/boost_1_65_1
 ./bootstrap.sh
 sed -i "s/using gcc ;/using gcc : x86_64 : x86_64-w64-mingw32-g++ ;/g" project-config.jam
-./bjam install toolset=gcc-mingw --prefix=$HOME/mingw-bin target-os=windows variant=release
+./bjam install toolset=gcc-mingw --prefix=$HOME/mingw-bin target-os=windows variant=release variant=release --without-mpi address-model=64 architecture=x86 release abi=ms binary-format=pe --with-thread
+wget "https://raw.githubusercontent.com/bunkercoin-project/bunkercoin/1.0-dev/contrib/windows-boost-thread/libboost_thread.a"
+mv libboost_thread.a $HOME/mingw-bin/lib
 
 # Install packages
 cd $HOME/mingw-bin
