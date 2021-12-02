d7beff79e206fd599f23b6fb97c209fc9fb7de67
Bram <84628826+sys-256@users.noreply.github.com>
2021-12-02 11:22:50 +0100

    Revert some changes

--- contrib/compilepackages-unix.sh
+++ contrib/compilepackages-unix.sh
@@ -12,47 +12,63 @@ sudo apt purge libdb-dev libdb++-dev -y
 
 # Go to home directory and make bin dirs
 cd $HOME
-mkdir -p unix-bin/include/miniupnpc
-mkdir -p unix-bin/lib
+mkdir boost-bin
+mkdir ssl-bin
+mkdir -p upnp-bin/include/miniupnpc
+mkdir -p upnp-bin/lib
+mkdir db-bin
 
 # Get source code
+wget "https://boostorg.jfrog.io/artifactory/main/release/1.65.1/source/boost_1_65_1.tar.gz"
 wget "https://www.openssl.org/source/old/1.0.2/openssl-1.0.2u.tar.gz"
 wget "http://miniupnp.free.fr/files/miniupnpc-2.0.20161216.tar.gz"
 wget "http://download.oracle.com/berkeley-db/db-5.1.29.NC.tar.gz"
-wget "https://deac-ams.dl.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.gz"
 
 # Extract source code
+tar -xf boost_1_65_1.tar.gz
 tar -xf openssl-1.0.2u.tar.gz
 tar -xf miniupnpc-2.0.20161216.tar.gz
 tar -xf db-5.1.29.NC.tar.gz
-tar -xf boost_1_55_0.tar.gz
 
-rm -f openssl-1.0.2u.tar.gz miniupnpc-2.0.20161216.tar.gz db-5.1.29.NC.tar.gz boost_1_55_0.tar.gz
+# Delete source code archives
+rm -f boost_1_65_1.tar.gz openssl-1.0.2u.tar.gz miniupnpc-2.0.20161216.tar.gz db-5.1.29.NC.tar.gz
+
+# Build libboost
+cd $HOME/boost_1_65_1
+./bootstrap.sh --prefix=$HOME/boost-bin
+./b2 install
 
 # Build OpenSSL
 cd $HOME/openssl-1.0.2u
-./config --prefix=/usr
+./config shared zlib --prefix=$HOME/ssl-bin
 make -j`nproc`
-sudo make install
+make install
 
 # Build miniupnpc
 cd $HOME/miniupnpc-2.0.20161216
 make -j`nproc`
-cp libminiupnpc.a libminiupnpc.so $HOME/unix-bin/lib
-cp *.h $HOME/unix-bin/include/miniupnpc
+cp libminiupnpc.a $HOME/upnp-bin/lib
+cp *.h $HOME/upnp-bin/include/miniupnpc
 
 # Build Berkeley DB
 cd $HOME/db-5.1.29.NC/build_unix/
 sed -i 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' ../src/dbinc/atomic.h
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$HOME/unix-bin
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$HOME/db-bin
 make -j`nproc`
 make install
 
-# Build libboost
-cd $HOME/boost_1_55_0
-./bootstrap.sh --prefix=$HOME/unix-bin
-./b2 install
+# Install libboost
+cd $HOME/boost-bin
+sudo cp * -R /usr
 
-# Install packages
-cd $HOME/unix-bin
+# Install OpenSSL
+cd $HOME/ssl-bin
+sudo cp * -R /usr
+
+# Install miniupnpc
+cd $HOME/upnp-bin
+sudo cp * -R /usr
+
+# Install Berkeley DB
+cd $HOME/db-bin 
 sudo cp * -R /usr
