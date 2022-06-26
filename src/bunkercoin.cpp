1ffa93a1bec838c9fcd7bd825033112411e35566
IdotMaster1 <justapersom@gmail.com>
2022-06-26 23:27:21 +0200

    lol jk just trolling

--- src/bunkercoin.cpp
+++ src/bunkercoin.cpp
@@ -134,13 +134,13 @@ CAmount GetDogecoinBlockSubsidy(int nHeight, const Consensus::Params& consensusP
         const char* cseed = cseed_str.c_str();
         char* endp = NULL;
         long seed = strtol(cseed, &endp, 16);
-        CAmount maxReward = (1000000 >> (4*halvings)) - 1; 
+        CAmount maxReward = (1000000 >> (4*halvings)) - 1; sss
         int rand = generateMTRandom(seed, maxReward);
 
         return (1 + rand) * COIN;
     } else if (nHeight < (6 * consensusParams.nSubsidyHalvingInterval)) {
         // New-style constant rewards for each halving interval
-        return (125000 * COIN) >> halvings;
+        return (500000 * COIN) >> halvings;
     } else {
         // Constant inflation
         return 10000 * COIN;
