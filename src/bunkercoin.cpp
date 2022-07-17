e7b25422a1155b257724b3c5d2281664743646ff
IdotMaster1 <justapersom@gmail.com>
2022-07-17 21:33:58 +0200

    test

--- src/bunkercoin.cpp
+++ src/bunkercoin.cpp
@@ -130,40 +130,22 @@ CAmount GetDogecoinBlockSubsidy(int nHeight, const Consensus::Params& consensusP
 
     if (!consensusParams.fSimplifiedRewards)
     {
-        // Old-style rewards derived from the previous block hash
-        /*
         const std::string cseed_str = prevHash.ToString().substr(7, 7);
         const char* cseed = cseed_str.c_str();
         char* endp = NULL;
         long seed = strtol(cseed, &endp, 16);
         CAmount maxReward = (1000000 >> halvings) - 1;
         int rand = generateMTRandom(seed, maxReward);
-        return (1 + rand) * COIN;
-        */
 
-       return 10000 * COIN;
-
-    } 
-    else if (nHeight < (2 * consensusParams.nSubsidyHalvingInterval)) {
-        // New stle halving that "halves" at 4x to calm the inflation disaster that happend
-        const std::string cseed_str = prevHash.ToString().substr(7, 7);
-        const char* cseed = cseed_str.c_str();
-        char* endp = NULL;
-        long seed = strtol(cseed, &endp, 16);
-        CAmount maxReward = (1000000 >> (4*halvings)) - 1; 
-        int rand = generateMTRandom(seed, maxReward);
         return (1 + rand) * COIN;
-    }
-    else if (nHeight < (3 * consensusParams.nSubsidyHalvingInterval)) {
+    } else if (consensusParams.fSimplifiedRewards == true) {
         // New-style constant rewards for each halving interval
         return (500000 * COIN) >> halvings;
-    } 
-    else {
+    } else {
         // Constant inflation
         return 10000 * COIN;
     }
 }
-
 
 
 
